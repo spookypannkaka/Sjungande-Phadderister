c8b18ab915c21148af1f9b80b3c17fe8bc7ed0c0
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2022-09-12 15:09:27 +0200

    Fungerande FFT-analys

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -4,17 +4,37 @@ let mic;
   let cnv = createCanvas(400, 400);
   cnv.mousePressed(userStartAudio);
   textAlign(CENTER);
+  fft = new p5.FFT();
   mic = new p5.AudioIn();
   mic.start();
+  fft.setInput(mic);
 }
 
 
 function draw() {
-  background(0);
-  fill(255);
-  text('tap to start', width/2, 20);
+  background(220);
+  //fill(255);
+  //text('tap to start', width/2, 20);
+  let spectrum = fft.analyze();
+  for (let i = 0; i< spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h )
+  }
 
-  micLevel = mic.getLevel();
+ /* micLevel = mic.getLevel();
   let y = height - micLevel * height;
-  ellipse(width/2, y, 10, 10);
+  ellipse(width/2, y, 10, 10);*/
 }
+
+/*function draw(){
+  background(220);
+
+    
+  noStroke();
+  fill(255, 0, 255);
+  for (let i = 0; i< spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h )
+  }*/
