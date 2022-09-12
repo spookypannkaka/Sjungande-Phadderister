188cde167b15d14cfdf3b3a496bc7a70e01793a9
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2022-09-12 14:40:55 +0200

    Fixat så vi har ljudinput
    yeah my guy

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  // put setup code here
+let mic;
+
+ function setup(){
+  let cnv = createCanvas(400, 400);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
+
 function draw() {
-  // put drawing code here
+  background(0);
+  fill(255);
+  text('tap to start', width/2, 20);
+
+  micLevel = mic.getLevel();
+  let y = height - micLevel * height;
+  ellipse(width/2, y, 10, 10);
 }
