8b40d0b8954addfef960afcd6552a5757f9fe732
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2022-09-15 16:18:28 +0200

    Update sketch.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -10,12 +10,19 @@ let mic;
   fft.setInput(mic);
 }
 
-
+//Fixa harmonic product spectrum... 
 function draw() {
   background(220);
   //fill(255);
   //text('tap to start', width/2, 20);
   let spectrum = fft.analyze();
+<<<<<<< Updated upstream
+=======
+  //Get energy ger energy (volym) vid specifika frekvenser eller mellan frekvenser. Tänker att man kanske kan använda för styrning?
+  let bassEnergy = fft.getEnergy("bass");
+  text(bassEnergy, width/2, 20);
+  
+>>>>>>> Stashed changes
   for (let i = 0; i< spectrum.length; i++){
     let x = map(i, 0, spectrum.length, 0, width);
     let h = -height + map(spectrum[i], 0, 255, height, 0);
