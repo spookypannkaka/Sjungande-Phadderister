2ac9ec1284c1151073a18bd0063ce25f002adcbe
rasmussvala <rassv453@student.liu.se>
2022-09-19 16:09:17 +0200

    städning

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -14,22 +14,14 @@ let mic;
 function draw() {
   background(249, 141 , 253);
   let spectrum = fft.analyze();
-  //Get energy ger energy (volym) vid specifika frekvenser eller mellan frekvenser. Tänker att man kanske kan använda för styrning?
-  let bassEnergy = fft.getEnergy("treble");
-  text(bassEnergy, width/2, 20);
+
+  var max_of_array = Math.max.apply(Math, spectrum);
+
+  text(max_of_array, width/2, 20);
   
   for (let i = 0; i< spectrum.length; i++){
     let x = map(i, 0, spectrum.length, 0, width);
     let h = -height + map(spectrum[i], 0, 255, height, 0);
     rect(x, height, width / spectrum.length, h )
   }
-
-
-  //Kod för boll som styrs (i y-led) av volym
-  /*micLevel = mic.getLevel();
-    for (let i = 0; i< spectrum.length; i++){
-    let y = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-    ellipse(width/2, y, 10, 10);
-  }*/
 }
