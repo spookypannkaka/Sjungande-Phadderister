6eb064929075de6937e90784896bc9c10d0ee7eb
rasmussvala <rassv453@student.liu.se>
2022-09-22 20:28:28 +0200

    fungerande downsample

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,6 +2,7 @@ let mic;
 
 function setup() {
 
+
   // create canvas
   createCanvas(400, 400);
 
@@ -16,16 +17,36 @@ function setup() {
 
 function draw() {
 
-  background(220);
+  background(0, 200, 0);
 
   let spectrum = fft.analyze();
+  
+  // konstanter 
+  const spectrumLength = spectrum.length;
+  const halfSpectrumLength = spectrum.length/2;
+  const quarterSpectrumLength = spectrum.length/4;
+
+  // arrayer som är till för downsampling  
+  const downsample1 = new Array(spectrumLength).fill(100); // byt till fill(0)
+  const downsample2 = new Array(spectrumLength).fill(100); // byt till fill(0)
+
+  // första downsample 
+  for(let i = 0; i < halfSpectrumLength; i++){
+    downsample1[i] = (spectrum[2*i] + spectrum[2*i+1])/2;
+  }
+
+  // andra downsample 
+  for(let i = 0; i < quarterSpectrumLength; i++){
+    downsample2[i] = (downsample1[2*i] + downsample1[2*i+1])/2;
+  }
 
   // draw the freq spectrum 
   noStroke();
   fill(255, 0, 255);
-  for (let i = 0; i< spectrum.length; i++){
+  
+  for (let i = 0; i< spectrumLength; i++){
     let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    let h = -height + map(downsample2[i], 0, 255, height, 0);
     rect(x, height, width / spectrum.length, h )
   }
 }
