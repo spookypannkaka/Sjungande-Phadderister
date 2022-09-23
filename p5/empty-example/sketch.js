0eb65aeb3dc3243cf640b5262f9e32ba6630b6bc
rasmussvala <rassv453@student.liu.se>
2022-09-23 08:50:54 +0200

    fungerade hps..?

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,10 +1,10 @@
-let mic;
+let mic, filter;
 
 function setup() {
-
+  
 
   // create canvas
-  createCanvas(400, 400);
+  createCanvas(1024, 700);
 
   // Initialize fft
   fft = new p5.FFT();
@@ -19,19 +19,32 @@ function draw() {
 
   background(0, 200, 0);
 
+  // analyserar frekvensspektrumet
   let spectrum = fft.analyze();
-  
+
   // konstanter 
   const spectrumLength = spectrum.length;
   const halfSpectrumLength = spectrum.length/2;
   const quarterSpectrumLength = spectrum.length/4;
 
+  // tar bort frek. < 50 Hz då det anses vara brus http://musicweb.ucsd.edu/~trsmyth/analysis/Harmonic_Product_Spectrum.html
+  const maxFreq = 22050; 
+  const cutoff = 50;
+  let interval = maxFreq/spectrumLength;
+  let index = cutoff/interval;
+ 
+  for(let i = 0; i < index; i++){
+    spectrum[i] = 0;
+  }
+
   // arrayer som är till för downsampling  
-  const downsample1 = new Array(spectrumLength).fill(100); // byt till fill(0)
-  const downsample2 = new Array(spectrumLength).fill(100); // byt till fill(0)
+  const downsample1 = new Array(spectrumLength).fill(0); // byt till fill(0)
+  const downsample2 = new Array(spectrumLength).fill(0); // byt till fill(0)
+  const product = new Array(spectrumLength).fill(0); // byt till fill(0)
 
   // första downsample 
-  for(let i = 0; i < halfSpectrumLength; i++){
+  for(let i = 0; i < halfSpectrumLength; i++){ 
+    // beräknar medelvärdet mellan två indexes och sparar det till downsample arrayn
     downsample1[i] = (spectrum[2*i] + spectrum[2*i+1])/2;
   }
 
@@ -40,13 +53,19 @@ function draw() {
     downsample2[i] = (downsample1[2*i] + downsample1[2*i+1])/2;
   }
 
+  // beräknar produkten av alla spektrum
+  for(let i = 0; i < spectrumLength; i++){
+    product[i] = spectrum[i] * downsample1[i] * downsample2[i];
+    product[i] = product[i]/5000;
+  }
+
   // draw the freq spectrum 
   noStroke();
   fill(255, 0, 255);
   
   for (let i = 0; i< spectrumLength; i++){
     let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(downsample2[i], 0, 255, height, 0);
+    let h = -height + map(product[i], 0, 255, height, 0);
     rect(x, height, width / spectrum.length, h )
   }
 }
