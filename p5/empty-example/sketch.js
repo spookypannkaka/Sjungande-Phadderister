c0e315e3b91fc8e6977d7209a06b31b7efc0653f
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2022-09-29 14:28:31 +0200

    ljud grejer inlagda
    de gör inget just nu dou

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -11,6 +11,13 @@ var startScreenTitle;
 var title;
 var startScreenContainer;
 
+// Audio stuff
+let mic;
+const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/'
+let pitch;
+let audioContext;
+let freq = 0;
+
 // All buttons
 var startPlay;
 var startPlay2;
@@ -101,9 +108,44 @@ function preload() {
 
 function setup() {
   //canvas = createCanvas(screenWidth, screenHeight); // This sets the "size" of the game. It's 960x540 pixels (16:9 aspect ratio)
-  createCanvas(1440, 810); // This sets the "size" of the game. It's 960x540 pixels (16:9 aspect ratio)
+  let cnv = createCanvas(1440, 810); // This sets the "size" of the game. It's 960x540 pixels (16:9 aspect ratio)
   //pixelDensity(1);
   //image(startScreenBG,0,0).hide();
+  audioContext = getAudioContext();
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  fft = new p5.FFT();
+  mic = new p5.AudioIn();
+  mic.start(listening);
+  fft.setInput(mic);
+}
+
+//Laddar in pitchdetection från ml5 samt skriver ut ddet till konsollen
+function listening(){
+  console.log('Listening...');
+  pitch = ml5.pitchDetection(
+    model_url,
+    audioContext,
+    mic.stream,
+    modelLoaded);
+}
+
+//Laddar modellen (oklart varför man måste göra detta?)
+function modelLoaded() {
+  console.log('model loaded');
+  pitch.getPitch(gotPitch);
+}
+
+//Funktionen som kollar om vi har en frekvens, om nej så loggar den ett error, om vi har fått in en frekvens sätts
+//Variabeln freq till vår frekvens sen körs den igen (basically en oändlig loop atm)
+function gotPitch(error, frequency){
+  if(error){
+    console.log(error);
+  }
+  if(frequency){
+    freq = frequency;
+  }
+  pitch.getPitch(gotPitch);
 }
 
 function draw() {
@@ -408,3 +450,28 @@ function reset(){
   	speed=2;
   	y=-20;
 }
+
+/*//Fixa harmonic product spectrum... 
+function draw() {
+  let volume = mic.getLevel();
+  let threshold = 0.007;
+  console.log("volyme is " + volume);
+  background(43, 236, 82);
+  //Teststorlek samt freq.toFixed skriver ut frekvensen
+  textSize(64);
+  if(volume > threshold){
+    text(freq.toFixed(2), width/2, height/2);
+    //console.log(freq);
+  }
+  else{
+    text("0",width/2, height/2);
+  }
+  //FFTn (som egentligen inte ens används)
+  let spectrum = fft.analyze();
+  for (let i = 0; i< spectrum.length; i++){
+    fill(0, 0, 255);
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h )
+  }
+}*/
